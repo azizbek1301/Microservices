8979e745986fe3ca5ec5d34c4f8298fe782394d5
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-05 18:53:28 +0500

    student View model

---
+++ EduCenter.Api/ViewModels/StudentDto.cs
@@ -0,0 +1,16 @@
+﻿using EduCenter.Domain.Enums;
+
+namespace EduCenter.Api.ViewModels
+{
+    public class StudentDto
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public Role Role { get; set; }
+        public string Email { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
