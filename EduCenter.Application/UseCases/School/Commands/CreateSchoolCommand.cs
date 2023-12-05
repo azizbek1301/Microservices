446793d29f84bbf265bac14e826eb19d2bd34137
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-05 17:48:39 +0500

    school Commands

---
+++ EduCenter.Application/UseCases/School/Commands/CreateSchoolCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace EduCenter.Application.UseCases.School.Commands
+{
+    public class CreateSchoolCommand:IRequest
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; } 
+    }
+}
---
+++ EduCenter.Application/UseCases/School/Commands/UpdateSchoolCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCenter.Application.UseCases.School.Commands
+{
+    public class UpdateSchoolCommand:IRequest<bool>
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
