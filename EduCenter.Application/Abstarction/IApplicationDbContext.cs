b4c15e72685e93739181b0076d86645f02ece44e
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-05 17:48:25 +0500

    Abstraction

---
+++ EduCenter.Application/Abstarction/IApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EduCenter.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduCenter.Application.Abstarction
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<School> School { get; set; }
+        public DbSet<Group> Group { get; set; }
+        
+        public DbSet<Room> Room { get; set; }
+        public DbSet<Student> Student { get; set; }
+        
+        public DbSet<Teacher> Teacher { get; set; }
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+    }
+}
