41ddc3cabc8bf60740397b33ca5d4124a2213290
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-05 17:49:10 +0500

    school queries

---
+++ EduCenter.Application/UseCases/School/Queries/GetSchoolByIdCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCenter.Application.UseCases.School.Queries
+{
+    public class GetSchoolByIdCommand:IRequest<Domain.Entities.School>
+    {
+        public int Id { get; set; }
+
+    }
+}
---
+++ EduCenter.Application/UseCases/School/Queries/GetSchoolCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCenter.Application.UseCases.School.Queries
+{
+    public class GetSchoolCommand:IRequest<List<Domain.Entities.School>>
+    {
+
+    }
+}
