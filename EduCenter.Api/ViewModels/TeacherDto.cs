2d673968907fbc0da30033242275f90c673d11dc
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-05 17:48:01 +0500

    teacher dto

---
+++ EduCenter.Api/ViewModels/TeacherDto.cs
@@ -0,0 +1,14 @@
+﻿using EduCenter.Domain.Enums;
+
+namespace EduCenter.Api.ViewModels
+{
+    public class TeacherDto
+    {
+        public int TeacherId { get; set; }  
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public Role Role { get; set; }
+        public int SchoolId { get; set; }
+    }
+}
