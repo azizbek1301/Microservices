5c06d09417ccf64b4d3a0d9cf06acfaa91851b64
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-05 17:50:55 +0500

    teacherCommands

---
+++ EduCenter.Application/UseCases/Teacher/Commands/CreatTeacherCommand.cs
@@ -0,0 +1,19 @@
+﻿using EduCenter.Domain.Enums;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCenter.Application.UseCases.Teacher.Commands
+{
+    public class CreatTeacherCommand:IRequest
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public Role Role { get; set; }
+        public int SchoolId { get; set; }
+    }
+}
---
+++ EduCenter.Application/UseCases/Teacher/Commands/UpdateTeacherCommand.cs
@@ -0,0 +1,20 @@
+﻿using EduCenter.Domain.Enums;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCenter.Application.UseCases.Teacher.Commands
+{
+    public class UpdateTeacherCommand:IRequest<bool>
+    {
+        public int TeacherId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public Role Role { get; set; }
+        public int SchoolId { get; set; }
+    }
+}
