bef9b1b82704b9f672d6b352b20131831c15390f
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-05 18:54:37 +0500

    Student Queries

---
+++ EduCenter.Application/UseCases/Student/Queries/GetAllStudentCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace EduCenter.Application.UseCases.Student.Queries
+{
+    public class GetAllStudentCommand: IRequest<List<Domain.Entities.Student>>
+    {
+
+    }
+}
---
+++ EduCenter.Application/UseCases/Student/Queries/GetStudentByIdCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace EduCenter.Application.UseCases.Student.Queries
+{
+    public class GetStudentByIdCommand :IRequest<Domain.Entities.Student>
+    {
+        public int StudentId {  get; set; }
+    }
+}
