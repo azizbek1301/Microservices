124b2958411a358084fcfcfe57e39c6fab261842
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-05 17:47:39 +0500

    TeacherController added

---
+++ EduCenter.Api/Controllers/TeacherController.cs
@@ -0,0 +1,84 @@
+﻿using EduCenter.Api.ViewModels;
+using EduCenter.Application.UseCases.School.Commands;
+using EduCenter.Application.UseCases.School.Queries;
+using EduCenter.Application.UseCases.Teacher.Commands;
+using EduCenter.Application.UseCases.Teacher.Handlers;
+using EduCenter.Application.UseCases.Teacher.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduCenter.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        private readonly IMediator _mediatr;
+
+        public TeacherController(IMediator mediatr)
+        {
+            _mediatr = mediatr;
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateTeacherAsync(TeacherDto model)
+        {
+            var command = new CreatTeacherCommand
+            {
+                Name = model.Name,
+                LastName= model.LastName,
+                Phone= model.Phone,
+                Role= model.Role,
+                SchoolId= model.SchoolId,
+
+            };
+
+            await _mediatr.Send(command);
+
+            return Ok("Yaratildi");
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllTeacherAsync()
+        {
+            var teacher = await _mediatr.Send(new GetAllTeacherCommand());
+            return Ok(teacher);
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteById(int Id)
+        {
+            var command = new DeleteTeacherComman
+            {
+                TeacherId = Id
+            };
+            bool result = await _mediatr.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateTeacherAsync(TeacherDto model)
+        {
+            var teacher = new UpdateTeacherCommand()
+            {
+                
+                TeacherId=model.TeacherId,
+                LastName=model.LastName,                
+                Name = model.Name,
+                SchoolId = model.SchoolId,
+                Role= model.Role,
+                Phone = model.Phone
+            };
+
+            await _mediatr.Send(teacher);
+
+            return Ok("Updated");
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetTeacherByIdAsync(int Id)
+        {
+            var res = await _mediatr.Send(new GetTeacherByIdCommand {TeacherId = Id });
+            return Ok(res);
+        }
+    }
+}
