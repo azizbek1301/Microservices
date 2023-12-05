054a3d974c9337bd7fbf05963bbc09727514e384
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-05 17:47:51 +0500

    school Dto

---
+++ EduCenter.Api/ViewModels/SchoolDto.cs
@@ -0,0 +1,11 @@
+﻿namespace EduCenter.Api.ViewModels
+{
+    public class SchoolDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Address { get; set; } = default!;
+        public string Phone_Number { get; set; } = default!;
+        public string Email { get; set; } = default!;
+    }
+}
