f3ded97f77bcc46be8cd0ca15ded093bf52d31a9
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-06 20:10:33 +0500

    EduCenterViewModels

---
+++ EduCenter.Api/ViewModels/GroupDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EduCenter.Api.ViewModels
+{
+    public class GroupDto
+    {
+        public int Id { get; set; }
+        public string ClassNumber { get; set; }
+        public int StudentCount { get; set; }
+        public int SchoolId { get; set; }
+    }
+}
---
+++ EduCenter.Api/ViewModels/RoomDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EduCenter.Api.ViewModels
+{
+    public class RoomDto
+    {
+        public int Id { get; set; }
+        public int RoomNumber { get; set; }
+        public int SeatCount { get; set; }
+    }
+}
