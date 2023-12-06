1045a972ddb0692573b95b84e60ac5769c7013cc
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-06 20:11:23 +0500

    GroupQueries

---
+++ EduCenter.Application/UseCases/Group/Queries/GetAllGroupCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCenter.Application.UseCases.Group.Queries
+{
+    public class GetAllGroupCommand :IRequest<List<Domain.Entities.Group>>
+    {
+    }
+}
---
+++ EduCenter.Application/UseCases/Group/Queries/GetGroupByIdCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace EduCenter.Application.UseCases.Group.Queries
+{
+    public class GetGroupByIdCommand:IRequest<Domain.Entities.Group>
+    {
+        public int Id { get; set; }
+    }
+}
