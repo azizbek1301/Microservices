8dd0b010899b83435c5e59a0397c5f77093e6c29
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-07 03:35:14 +0500

    ApplicationDbContext

--- EduCenter.Infrastructure/Peristance/ApplicationDbContext.cs
+++ EduCenter.Infrastructure/Peristance/ApplicationDbContext.cs
@@ -2,6 +2,8 @@
 using EduCenter.Domain.Entities;
 using EduCenter.Infrastructure.Configurations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace EduCenter.Infrastructure.Peristance
 {
@@ -9,8 +11,37 @@ namespace EduCenter.Infrastructure.Peristance
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+            var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+            try
+            {
+                if (databaseCreator is null)
+                {
+                    throw new Exception("Database Not Found!");
+                }
+
+                if (!databaseCreator.CanConnect())
+                    databaseCreator.CreateAsync();
+
+                if (!databaseCreator.HasTables())
+                    databaseCreator.CreateTablesAsync();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=sqldata;Initial Catalog=EduCenterDb;User Id=SA;Password=Numsey#2022;Persist Security Info=True;;TrustServerCertificate=true", builder =>
+            {
+                builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+
         public DbSet<School> School { get; set; }
         public DbSet<Group> Group { get; set; }
         public DbSet<Room> Room { get; set; }
