ad8c675a58405e3fc729d27a4d3e095763e47a3c
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-06 20:12:31 +0500

    RoomQueries

---
+++ EduCenter.Application/UseCases/Room/Queries/GetAllRoomCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace EduCenter.Application.UseCases.Room.Queries
+{
+    public class GetAllRoomCommand :IRequest<List<Domain.Entities.Room>>
+    {
+
+    }
+}
---
+++ EduCenter.Application/UseCases/Room/Queries/GetRoomByIdCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace EduCenter.Application.UseCases.Room.Queries
+{
+    public class GetRoomByIdCommand : IRequest<Domain.Entities.Room>
+    {
+        public int Id { get; set; }
+    }
+}
