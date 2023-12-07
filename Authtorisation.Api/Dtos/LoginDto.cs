b95b83bfcaac160872c0a947c2aeccfae73a363c
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-07 17:31:39 +0500

    Authorisation

---
+++ Authtorisation.Api/Dtos/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Authtorisation.Api.Dtos
+{
+    public class LoginDto
+    {
+        public string UserName {  get; set; }
+        public string Password { get; set; }    
+    }
+}
---
+++ Authtorisation.Api/Services/AuthServices.cs
@@ -0,0 +1,32 @@
+﻿using Authtorisation.Api.Dtos;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Authtorisation.Api.Services
+{
+    public class AuthServices
+    {
+        public string GenerateToken(LoginDto loginDto)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("asosiyUser711w15ed16516w6e51de65f1ef1e6f51ef51"));
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, loginDto.UserName),
+                new Claim(ClaimTypes.Role, "User")
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "Issuer",
+                audience: "Audience",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: signingCredentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
