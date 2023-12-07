02d7a1c5c52c6c90bf674540363b06e2a7c03022
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-07 17:44:36 +0500

    Sport Infrastructure

---
+++ Sport.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sport.DataApplication.Abstraction;
+using Sport.Infrastructure.Peristance;
+
+namespace Sport.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("Default"));
+            });
+            return services;
+        }
+    }
+}
---
+++ Sport.Infrastructure/Peristance/ApplicationDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Sport.DataApplication.Abstraction;
+using Sport.Domain.Entities;
+
+namespace Sport.Infrastructure.Peristance
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+            try
+            {
+                if (databaseCreator is null)
+                {
+                    throw new Exception("Database Not Found!");
+                }
+
+                if (!databaseCreator.CanConnect())
+                    databaseCreator.CreateAsync();
+
+                if (!databaseCreator.HasTables())
+                    databaseCreator.CreateTablesAsync();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Data Source=sqldata;Initial Catalog=footballDb;User Id=SA;Password=Numsey#2022;Persist Security Info=True;;TrustServerCertificate=true", builder =>
+            //{
+            //    builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+            //});
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
