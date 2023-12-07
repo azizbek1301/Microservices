e8b92742fdb8ef4242b19b2d83612fcf41090920
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-07 17:45:47 +0500

    Sport Abstraction

---
+++ Sport.Service/Abstraction/IApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sport.Domain.Entities;
+
+
+namespace Sport.DataApplication.Abstraction
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+
+        public DbSet<Coach> Coaches { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+       
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
