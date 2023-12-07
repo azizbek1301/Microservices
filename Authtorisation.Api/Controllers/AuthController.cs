182df003374b69b7e023918415d95a7c792b405f
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-07 17:31:21 +0500

    AuthorController

---
+++ Authtorisation.Api/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Authtorisation.Api.Dtos;
+using Authtorisation.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authtorisation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthServices _tokenGenerator;
+
+        public AuthController(AuthServices tokenGenerator)
+        {
+            _tokenGenerator = tokenGenerator;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> Login(LoginDto login)
+        {
+            var token = _tokenGenerator.GenerateToken(login);
+            return Ok(token);
+        }
+    }
+}
