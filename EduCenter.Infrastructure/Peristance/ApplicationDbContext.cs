3461502e8efa7c382bbaaebc5fa53f49041fbc4d
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-08 15:22:26 +0500

    Application DbContext EduCenter

--- EduCenter.Infrastructure/Peristance/ApplicationDbContext.cs
+++ EduCenter.Infrastructure/Peristance/ApplicationDbContext.cs
@@ -10,25 +10,7 @@ namespace EduCenter.Infrastructure.Peristance
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
-            try
-            {
-                if (databaseCreator is null)
-                {
-                    throw new Exception("Database Not Found!");
-                }
-
-                if (!databaseCreator.CanConnect())
-                    databaseCreator.CreateAsync();
-
-                if (!databaseCreator.HasTables())
-                    databaseCreator.CreateTablesAsync();
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+           
 
         }
 
