0f81fc831955d4c9aea29bf7045f1d062f8b355d
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2023-12-09 13:41:50 +0500

    Auth Dto

--- Authtorisation.Api/Dtos/LoginDto.cs
+++ Authtorisation.Api/Dtos/LoginDto.cs
@@ -1,8 +1,8 @@
 ﻿namespace Authtorisation.Api.Dtos
 {
-    public class LoginDto
+    public class LoginDTO
     {
-        public string UserName {  get; set; }
-        public string Password { get; set; }    
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
---
+++ Authtorisation.Api/Dtos/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Authtorisation.Api.Dtos
+{
+    public class UserDTO
+    {
+        public string UserName {  get; set; }
+        public string Role { get; set; }    
+    }
+}
